52c5cec124395cfa1ff582a788a205bf5dd65f67
Camila Richter <ric23183@uvg.edu.gt>
2026-01-14 18:42:39 -0600

    comentarios de las funciones

--- Environment.py
+++ Environment.py
@@ -6,10 +6,10 @@ class Environment:
     def init(self):
         self.actual_temp = random.randit(0,100) #inicializa la temperatura en un número random
 
-    def get_percept(self):
-        return self.actual_temp #devuelve temperatura actual
+    def get_percept(self): #devuelve temperatura actual
+        return self.actual_temp 
 
-    def update(self, action):
+    def update(self, action): #modifica la temperatura actual
         if action == "enfriar":
             self.actual_temp -= 1
         elif action == "calentar":
